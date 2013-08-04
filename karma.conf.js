f878342537c8d10aa4f993fec486ab22a614fdf5
Emanuel <kraemerseele@gmx.de>
2013-08-04 17:45:14 +0200

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = [];
+browsers = ['Chrome'];
 
 
 // If browser does not capture in given timeout [ms], kill it
